c96c9ac045e83aed7e5e3fa33f54ad27876a04bd
3n3a <46775561+3n3a@users.noreply.github.com>
2022-03-12 17:52:08 +0100

    Update index.php

--- backend/src/index.php
+++ backend/src/index.php
@@ -5,7 +5,7 @@ namespace M133;
 require_once __DIR__ . '/lib/Router.php';
 require_once __DIR__ . '/lib/TemplateY.php';
 
-use M133\Router as Router;
+use M133\RRouter as Router;
 use M133\TemplateY as Tpl;
 
 $views_base = __DIR__ . '/frontend/views/';
